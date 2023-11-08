db30e3db3e9b83df0fdf3bcaa57c11d714943525
Faranosh Amini <faranosh.amini4@gmail.com>
2023-11-09 03:59:19 +0430

    Add react components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./components/App";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
